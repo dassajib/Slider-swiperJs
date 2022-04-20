554434983d4207b7a4a6499b2124990f4cc5069c
Sajib <45949826+dassajib@users.noreply.github.com>
2022-04-20 23:29:34 +0600

    This is a slider where used swiperJs

---
+++ Swiper-Slide/script.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 1,
+    slidesPerView: 3,
+    // centeredSlides: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+
+    },
+});
